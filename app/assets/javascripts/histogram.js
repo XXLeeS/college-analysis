c459ca8e78f8195603759be742a9e91dd016c449
XXLeeS <leechinghon@gmail.com>
2016-12-25 05:39:46 -0800

    update histogram

--- app/assets/javascripts/histogram.js
+++ app/assets/javascripts/histogram.js
@@ -36,7 +36,7 @@ d3.json("/departments.json", function(dep){
 	var heights = hist_data.map(function(a) {return a.y});
 	var yScale = d3.scale.linear()
 				.domain([d3.min(heights),d3.max(heights)])
-				.range([20, max_height]);
+				.range([2, max_height]);
 	var rect_width = (svg_width-svg_padding*2) / bin_num;
 
 	var xAxis = d3.svg.axis()
@@ -66,13 +66,65 @@ d3.json("/departments.json", function(dep){
 					})
 					.attr("fill", function(d, i){
 						return colorScale(d.y);
+					})
+					.on("mouseover", function(d, i){
+						d3.select(this).attr("fill", "#E67E22");
+						d3.select(labels[0][i]).style("visibility", "visible");
+						d3.select(label_rects[0][i]).style("visibility", "visible");
+					})
+					.on("mouseout", function(d, i){
+						d3.select(this).attr("fill", colorScale(d.y));
+						d3.select(labels[0][i]).style("visibility", "hidden");
+						d3.select(label_rects[0][i]).style("visibility", "hidden");
 					});
+
+	var labels = svg.append("g")
+					.classed("labels", true)
+					.selectAll("text")
+					.data(hist_data)
+					.enter()
+					.append("text")
+					.attr("transform", "translate(" + svg_padding + "," + svg_padding + ")")
+					.attr("x", function(d, i){
+						return rect_width * (i);
+					})
+					.attr("y", function(d, i){
+						return max_height - yScale(d.y);
+					})
+					.attr("dx", function(){
+						return rect_width/2;
+					})
+					.attr("dy", -5)
+					.attr("text-anchor", "middle")
+					.text(function(d){
+			            return d.length + ", " + ((d.length / dep.length)*100).toFixed(1) + "%";
+			        })
+			        .style("fill", "#fff")
+			        .style("visibility", "hidden");
+
+	var labels_padding = 2;
+	var bboxs = [];
+	labels.each(function(){
+		bboxs.push(this.getBBox());
+	})
+
+	var label_rects = d3.select('.labels')
+						.selectAll("rect")
+						.data(bboxs)
+						.enter()
+						.insert("rect", ":first-child")
+						.attr("transform", "translate(" + svg_padding + "," + svg_padding + ")")
+						.attr("x", function(d){ return d.x - labels_padding })
+					    .attr("y", function(d){ return d.y })
+					    .attr("width", function(d){ return d.width + 2*labels_padding })
+					    .attr("height", function(d){ return d.height })
+					    .style("fill", "#D35400")
+					    .style("visibility", "hidden");
 
 	svg.append("g")
 	    .attr("class", "axis")
 	    .attr("transform", "translate(" + svg_padding + "," + (svg_height-svg_padding) + ")")
 	    .call(xAxis);
-
 
 	svg.append("line")
 		.attr("stroke", "#999")
