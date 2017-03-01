f0544ce727cc7f3f5ec3802b05749651704a6c5d
XXLeeS <leechinghon@gmail.com>
2017-03-01 22:51:41 +0800

    change year 87% complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def change_year
+  	url = request.referrer.split('/')
+
+    #http://domain.name/:year/...
+  	url[3] = params[:new_year]
+  	url = url.join('/')
+
+		redirect_to url
+		rescue ActionController::RedirectBackError
+			redirect_to root_path
+  end
 end
